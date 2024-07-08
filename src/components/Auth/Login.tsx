7b1aee35f15b6be790dc6cb6b90fa46b385ccf87
Franklin Tochukwu <tchfrnkln@gmail.com>
2024-07-08 10:24:00 +0100

    main button check

--- src/components/Auth/Login.tsx
+++ src/components/Auth/Login.tsx
@@ -1,16 +1,27 @@
 import React from 'react'
 import './styles.css';
-import { useInitData } from '@tma.js/sdk-react';
+import { initCloudStorage, initMainButton, useInitData } from '@tma.js/sdk-react';
 import Link from 'next/link';
 import Image from 'next/image';
 import Cart from "../../../assets/image/cart.svg"
 
 export const LoginUser = () => {
   const initData = useInitData();
+  const cloudStorage = initCloudStorage();
+  const [mainButton] = initMainButton();
+
+
+  // cloudStorage
+  // .set('name', 'Franklin')
+  // .then(() => console.log('Item saved'));
+
+  mainButton.hide();
+
+
 
   return (
     <div className='root'>
-      <Image alt='cart' src={require("../../../assets/image/cart.svg")} height={100}/>
+      <Image alt='cart' src={Cart} height={100}/>
       <h2 className='user'>{initData?.user?.username}</h2>
 
       <Link className='button' href="./mine-cstz">Start Mining CSTZ</Link>
